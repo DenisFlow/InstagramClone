f7ea35cf1bed83dd508ac2ca8eb99a021fe794af
DenisFlow <deo9flow@gmail.com>
2021-01-12 13:21:03 +0300

    error tabs

--- app/src/main/java/com/example/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.java
@@ -1,7 +1,5 @@
 package com.example.instagramclone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +8,8 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -98,8 +98,6 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
     public void rootLayoutTapped(View View){
         try {
-
-
             InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
             inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }catch (Exception e){
