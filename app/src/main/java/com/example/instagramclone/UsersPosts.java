f379a1a272beb95810e63157c68a29dfc484c196
DenisFlow <deo9flow@gmail.com>
2021-01-12 22:03:11 +0300

    user posts beginnig

---
+++ app/src/main/java/com/example/instagramclone/UsersPosts.java
@@ -0,0 +1,14 @@
+package com.example.instagramclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UsersPosts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_posts);
+    }
+}
